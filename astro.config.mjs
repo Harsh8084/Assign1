f5caa7cd76fab357f25312b10494ad2938aeded3
Harsh8084 <111571087+Harsh8084@users.noreply.github.com>
2023-07-04 22:40:05 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Harsh8084.github.io',
+    base: '/Assign1',
+});
